2fc3b6a0e11010e46f86b533e5f9e35b61c0d5f4
Outiszable <outsizable1.2@gmail.com>
2025-01-02 18:15:02 -0500

    hello

--- src/main/java/net/outsizable/alchainium/Alchainium.java
+++ src/main/java/net/outsizable/alchainium/Alchainium.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// hiiiii
 public class Alchainium implements ModInitializer {
 	public static final String MOD_ID = "alchainium";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
